e02def43de41de4b5150e826a28500ec52226692
Mersopolis <mersopolis@gmail.com>
2026-04-22 14:15:06 -0600

    Merge from predeployment

---
+++ devon-ethington-portfolio/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  output: 'export', // Outputs a Single-Page Application (SPA)
+  distDir: 'build', // Changes the build output directory to `build`
+}
+
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+  basePath: ''
+}
+ 
+export default nextConfig
