c4b59d707e85899ed51218da9bdd4cfc51e2c462
Mersopolis <mersopolis@gmail.com>
2026-04-22 21:35:16 -0600

    Trying Actions

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
+  basePath: process.env.PAGES_BASE_PATH,
 }
 
 module.exports = {
   images: {
     unoptimized: true,
-  },
-  basePath: ''
+  }
 }
  
 export default nextConfig
