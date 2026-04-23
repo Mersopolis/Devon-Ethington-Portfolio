0954d0696a4547dbc6f4ced57bed6e9f30ea0a0f
Mersopolis <mersopolis@gmail.com>
2026-04-22 20:55:04 -0600

    Wiping the slate

--- app/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import PortfolioContainer from './components/PortfolioContainer';
-
-const App = () => 
-    <div className="portfolio-app">
-      <PortfolioContainer />
-    </div>;
-
-export default App;
