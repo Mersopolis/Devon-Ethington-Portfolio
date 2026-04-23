3b316456c288cc1f2e4e3d9402a4deeff4f2bc5d
Mersopolis <mersopolis@gmail.com>
2026-04-23 12:11:25 -0600

    Updated Contact page, hid form

--- app/components/pages/Contact.js
+++ app/components/pages/Contact.js
@@ -1,17 +1,17 @@
-import ContactForm from "../ContactForm";
+// import ContactForm from "../ContactForm";
 
 function Contact() {
   return (
     <section id="contact">
       <h2>Contact Page</h2>
       <div id="contact-page">
-        <div id="contact-form">
+        {/* <div id="contact-form">
         <h3>Contact Form</h3>
         <ContactForm />
-        </div>
+        </div> */}
         <ul id="contact-info">
-          <li><h3>Or email me directly:</h3></li>
           <li>Email: <a href="mailto:mersopolis@gmail.com">mersopolis@gmail.com</a></li>
+          <li>Phone - Call or Text: <a href="tel:8018368141">(801)836-8141</a></li>
         </ul>
       </div>
     </section>
